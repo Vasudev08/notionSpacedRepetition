9d6d1b51aa57a3e55d9291c1e41d1bed85963e97
uuz <uuz@tamu.edu>
2024-03-23 10:01:36 -0500

    add dashboard display file

---
+++ src/dashboard.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+import './dashboard.css';
+import App from './App';
+
+function dashboard() {
+    // display dashboard
+    // agenda view with date of today, tmrw, and day after tmrw
+    // if matches date of any question in Notion database, display
+    
+    // session number only increases when a problem is marked done
+    // TODO: what to do with session number?
+
+    
+}
