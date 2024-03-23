7db2e8846f8303cc6c7ff485021134d31e969163
uuz <uuz@tamu.edu>
2024-03-23 09:30:04 -0500

    beautify

--- src/readDataNotion.js
+++ src/readDataNotion.js
@@ -4,16 +4,16 @@ const notion = new Client({
   auth: "secret_aW1fuyd9t0gpB8p0tEGcldTVxxedrThdo9N5dlzHCdB",
 })
 
-;(async () => {
-  const res = await notion.databases.query({
-    database_id: "59ace68d481741e3bb96c9574be6c6b4",
-    filter: {
-      property: "Date",
-      date: {
-        equals: "2024-03-21",
+  ; (async () => {
+    const res = await notion.databases.query({
+      database_id: "59ace68d481741e3bb96c9574be6c6b4",
+      filter: {
+        property: "Date",
+        date: {
+          equals: "2024-03-21",
+        },
       },
-    },
-  })
+    })
 
-  console.log(res)
-})()
+    console.log(res)
+  })()
