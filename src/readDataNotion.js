3e359badde613b3c7c9832680849d6653d419fff
Vasudev08 <vasu14devagarwal@gmail.com>
2024-03-23 09:06:09 -0500

    added notion example

---
+++ src/readDataNotion.js
@@ -0,0 +1,19 @@
+const { Client } = require("@notionhq/client")
+
+const notion = new Client({
+  auth: "secret_aW1fuyd9t0gpB8p0tEGcldTVxxedrThdo9N5dlzHCdB",
+})
+
+;(async () => {
+  const res = await notion.databases.query({
+    database_id: "59ace68d481741e3bb96c9574be6c6b4",
+    filter: {
+      property: "Date",
+      date: {
+        equals: "2024-03-21",
+      },
+    },
+  })
+
+  console.log(res)
+})()
